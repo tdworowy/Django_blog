f367e40125b0c221ffa6a0afca8646ceb983dc45
Tomasz <dworowytomasz@gmail.com>
2018-03-17 16:57:23 +0100

    fix

--- Example/Example1/blog/Utils/startServer.py
+++ Example/Example1/blog/Utils/startServer.py
@@ -4,7 +4,7 @@ import sys
 from Example.Utils.runFromShell import run
 
 if __name__ == '__main__':
-    if len(sys.argv) < 1:
+    if len(sys.argv) < 2:
         port = 8082
     else:
         port = sys.argv[1]
