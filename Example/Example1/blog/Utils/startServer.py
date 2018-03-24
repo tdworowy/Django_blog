166f8aa3dc153054198741ba5a57d3ad301ebe68
Tomasz <dworowytomasz@gmail.com>
2018-03-24 10:25:05 +0100

    fix

--- Example/Example1/blog/Utils/startServer.py
+++ Example/Example1/blog/Utils/startServer.py
@@ -5,7 +5,7 @@ from Example.Utils.runFromShell import run
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        port = 8083
+        port = '0.0.0.0:8083'
     else:
         port = sys.argv[1]
-    run(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..')),"runserver "+str(port))
+    run(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')), "runserver " + str(port))
--- TestFile.py
+++ TestFile.py
@@ -1,6 +1,13 @@
 import requests
+import sys
+
+if len(sys.argv) < 2:
+    ip = 'localhost'
+else:
+    ip = sys.argv[1]
+
 try:
-    print(requests.get("http://localhost:8083/blog/").text)
+    print(requests.get("http://%s:8083/blog/" % ip).text)
 except Exception as ex:
     print(ex)
-    print(requests.get("http://localhost:8083").text)
+    print(requests.get("http://%s:8083" % ip).text)
