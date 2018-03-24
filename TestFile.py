639833480e28d002f02871941f809189253388b7
Tomasz <dworowytomasz@gmail.com>
2018-03-24 09:11:40 +0100

    update

--- TestFile.py
+++ TestFile.py
@@ -1,6 +1,6 @@
 import requests
 try:
-    print(requests.get("http://localhost:8083/blog").text)
+    print(requests.get("http://localhost:8083/blog/").text)
 except Exception as ex:
     print(ex)
     print(print(requests.get("http://localhost:8083").text))
