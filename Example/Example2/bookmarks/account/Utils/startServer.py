331e855662eac49e45dbaac86a6db5bced85d60d
Tomasz <dworowytomasz@gmail.com>
2019-09-09 14:15:54 +0200

    clean up

--- Example/Example2/bookmarks/account/Utils/startServer.py
+++
@@ -1,6 +0,0 @@
-import os
-
-from Example.Utils.run_from_shell import run
-
-if __name__ == '__main__':
-    run(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..')),"runserver 8081")
--- Example/Example2/bookmarks/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib.auth import admin
-
-from Example.Example2.bookmarks.account import views
-
-urlpatterns = [
-    url(r'^login/$',views.user_login,name='login')
-
-]
